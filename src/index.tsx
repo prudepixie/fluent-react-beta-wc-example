6d6c20872fbaa62901486ac058ac842425a844a0
Wendy <wendy@Wendys-MacBook-Pro-Microsoft.local>
2022-02-18 12:29:54 -0800

    eject and working button

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import {
-//   FluentProvider,
-//   webLightTheme } from '@fluentui/react-components';
+import {
+  FluentProvider,
+  webLightTheme } from '@fluentui/react-components';
 import {  provideReactWrapper } from '@microsoft/fast-react-wrapper';
 
-// import { fluentButton } from './button';
-// import { provideFluentDesignSystem } from './fluent-design-system';
-
-import { fluentButton, provideFluentDesignSystem } from 'fluent-wc-v9';
-
-
+import {  fluentButton, provideFluentDesignSystem } from 'fluent-wc-v9';
 
 const { wrap } = provideReactWrapper(React);
-export const FluentButton = wrap(fluentButton().type, {name: 'fluent-button', properties: ['appearance']});
+export const FluentButton = wrap(fluentButton(), {name: 'fluent-button', properties: ['appearance']});
 
 provideFluentDesignSystem().register(fluentButton())
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <FluentProvider style={{ display: "grid", gap: "12px"}} theme={webLightTheme}> */}
-      <FluentButton>Button</FluentButton>
-    {/* </FluentProvider> */}
+    <FluentProvider style={{ display: "grid", gap: "12px"}} theme={webLightTheme}>
+      <FluentButton appearance="primary">Button</FluentButton>
+    </FluentProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
